45929f9e7e8779af9a33ea1a8abe70c505b9b1bb
cammorob <cammorob@gmail.com>
2023-04-08 21:20:15 -0500

    Unlink pages

---
+++ H Gates Managment  System/PatientsEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace H_Gates_Managment__System
+{
+    public partial class PatientsEntry : Form
+    {
+        public PatientsEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
